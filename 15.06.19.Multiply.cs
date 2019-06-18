0fbcdbb95d9ca080473b257374e697892efd2a1a
seth20032 <test@test.com>
2019-06-18 22:08:18 +0100

    added more solutions from 15.06.19

---
+++ 15.06.19.Multiply.cs
@@ -0,0 +1,15 @@
+/*
+The code does not execute properly. Try to figure out why.
+
+*/
+
+public class CustomMath {
+    public static int multiply(int a, int b) {
+     
+       
+        return a * b;
+    }
+    
+    //multiply(1, "2");
+    
+}
---
+++ 15.06.19.ThirdAngleOfATriangle.cs
@@ -0,0 +1,22 @@
+/*
+
+You are given two angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested. */
+
+
+
+using System;
+
+public static class Kata
+{
+  public static int OtherAngle(int a, int b)
+  {
+  
+    return 180-(a+b);
+    
+    throw new NotImplementedException();
+  }
+}
