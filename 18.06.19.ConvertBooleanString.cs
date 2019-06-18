78cec6487a33dea0e36a316eaf0ce8f7cc09d94b
seth20032 <test@test.com>
2019-06-18 23:07:08 +0100

    solutions for 18.06.19

---
+++ 18.06.19.ConvertBooleanString.cs
@@ -0,0 +1,11 @@
+
+
+using System;
+
+public class kata
+{
+  public static string boolean_to_string(bool b)
+  {
+  return b.ToString();
+  }
+}
---
+++ 18.06.19.RemoveFirstandLastCharacter.cs
@@ -0,0 +1,18 @@
+
+
+using System;
+
+public class Kata
+{
+    public static string Remove_char(string s)
+    {
+
+      s.Remove(0);
+
+        Console.WriteLine(s);
+        Console.ReadLine();
+
+       
+       // Your Code
+    }
+}
