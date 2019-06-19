c172ca4d19db28ca8a231ac692705d3efbb77c70
seth20032 <test@test.com>
2019-06-19 21:44:22 +0100

    added solution from challenges done on 19.16.19

---
+++ 19.06.19.MakeUpperCase.cs
@@ -0,0 +1,13 @@
+/*Write function makeUpperCase.*/
+
+  public class Kata
+  {
+        public static string MakeUpperCase(string str)
+        {
+             //return "str to upperCase";
+             return str.ToUpper();
+        }
+
+  }
+
+
